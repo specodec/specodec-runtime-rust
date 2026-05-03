d739aaabf9d10fa89fa314f80f08311c744da2a6
yutingrui <yutingrui@tal>
2026-05-03 23:50:18 +0800

    Export all public types; restructure interop tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ pub mod gron_reader;
 pub mod gron_writer;
 pub mod spec_reader;
 pub mod spec_writer;
+pub mod spec_writer_impls;
+pub mod spec_codec;
 pub mod msgpack_reader;
 pub mod msgpack_writer;
 pub mod float_fmt;
@@ -11,3 +13,14 @@ pub mod ryu;
 
 pub use ryu::float32_to_string;
 pub use ryu::float64_to_string;
+
+pub use json_reader::JsonReader;
+pub use json_reader::SCodecError;
+pub use json_writer::JsonWriter;
+pub use gron_reader::GronReader;
+pub use gron_writer::GronWriter;
+pub use msgpack_reader::MsgPackReader;
+pub use msgpack_writer::MsgPackWriter;
+pub use spec_reader::SpecReader;
+pub use spec_writer::SpecWriter;
+pub use spec_codec::SpecCodec;
